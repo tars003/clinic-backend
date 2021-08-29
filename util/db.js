ceebaca745405176ffb1d3feb2338ac93b13a92c
tars003 <sharmaajay2kuu@gmail.com>
2021-08-29 19:17:10 +0530

    added local mongo creds

--- util/db.js
+++ util/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
+const url = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`;
+
+const dbUrl = process.env.ENVIRONMENT == 'DEV' ? process.env.MONGO_URI : url;
+
 const connectDB = () => {
     mongoose
-        .connect(process.env.MONGO_URI, {
+        .connect(dbUrl, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
