1dac949dd0668e0086cb0fe09ec836c8d331c0b2
tars003 <sharmaajay2kuu@gmail.com>
2021-09-22 12:29:48 +0530

    added file for follow performa

---
+++ models/FollowImage.js
@@ -0,0 +1,14 @@
+const mongoose= require('mongoose');
+
+const imageSchema= mongoose.Schema(
+    {   
+        images : [
+            {
+                type: String
+            },
+        ],
+        appId: String
+    }
+);
+
+module.exports = mongoose.model("followImages",imageSchema)
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const Image = require('./models/Image');
 const multer=require('multer');
 const moment = require('moment');
 
 const connectDB = require('./util/db');
 const app = express();
 connectDB();
+
+const Image = require('./models/Image');
+const FollowImage  = require('./models/FollowImage');
 
 app.use(express.json());
 app.use('/server/public', express.static('public'));
@@ -37,6 +39,7 @@ var storage = multer.diskStorage({
 })
 
 var upload = multer({ storage: storage })
+
 app.post('/server/file/upload/:appointmentId', upload.single('myFile'), async (req, res, next) => {
     const file = req.file;
     console.log(file);
@@ -63,10 +66,43 @@ app.post('/server/file/upload/:appointmentId', upload.single('myFile'), async (r
         const savedimage = await imagepost.save()
         res.json(savedimage)
     }
+});
+
+app.post('/server/file/upload/follow-performa/:appointmentId', upload.single('myFile'), async (req, res, next) => {
+    const file = req.file;
+    console.log(file);
+    if (!file) {
+        const error = new Error('Please upload a file')
+        error.httpStatusCode = 400
+        return next("hey error")
+    }
+    let images = await FollowImage.findById(req.params.appointmentId);
+    if (images) {
+        images.images.push(file.path);
+
+        const newImages = await FollowImage.findById(images.id);
+        newImages.overwrite(images);
+        newImages.save();
+
+        res.json(newImages)
+    }
+    else {
+        const imagepost = new FollowImage({
+            images: [file.path],
+            _id: req.params.appointmentId
+        })
+        const savedimage = await imagepost.save()
+        res.json(savedimage)
+    }
 })
 
 app.get('/server/file/get-images/:appId', async (req, res) => {
     const image = await Image.findById(req.params.appId);
+    res.json(image)
+})
+
+app.get('/server/file/follow-performa/get-images/:appId', async (req, res) => {
+    const image = await FollowImage.findById(req.params.appId);
     res.json(image)
 })
 
