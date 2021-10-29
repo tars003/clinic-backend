96dd4f814235ee3b26325b5ef7e266ca1e360de8
tars003 <sharmaajay2kuu@gmail.com>
2021-10-29 11:18:29 +0530

    edit address, edit nationality, edit international fee

--- routes/patient.route.js
+++ routes/patient.route.js
@@ -293,7 +293,7 @@ router.post('/update', auth, async (req, res) => {
 
                 return res.status(200).json({
                     success: true,
-                    data: newProfile
+                    data: tempProfile
                 });
             }
             else {
